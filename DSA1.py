ce287188f5dca49e220a6002fbe41a8925a78895
bhanu-369 <chbhanuprakash369@gmail.com>
2026-03-28 19:53:47 +0530

    DSA_DAY1

---
+++ DSA1.py
@@ -0,0 +1,13 @@
+"""def delete(self,position_value):
+    #PREVIOUS POSITION
+    prev =self.head
+    #HEAD CALLING
+    temp =self.head.next
+    # ADD START POSITION USING LOOPS
+    for i in range(1,position_value):
+        #CURRENT POSITION &  CHECK MOVING NEXT POSITION 
+        temp=temp.next
+        #
+        prev=prev.next
+    prev.next=None"""
+    
